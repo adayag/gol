5f7bac9ec935fb64dbff975ac5f371f5f0796922
Allen Dayag <a.dayag@gmail.com>
2022-06-23 19:21:01 -0500

    Let's separate the board from the game

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,31 @@
 class Game
+  BOARD_SIZE = 30
   def initialize
     @iteration = 0
-    @board = Array.new(10) do
-      Array.new(10, "0")
-    end
+    @board = Board.new(BOARD_SIZE)
   end
 
   def print_board
-    @board.map do |cell|
+    @board.cells.map do |cell|
       puts cell.join(" ")
     end
   end
+end
+
+class Board
+  attr_reader :cells
+
+  def initialize(board_size)
+    @board_size = board_size
+    @cells = Array.new(10) do
+      Array.new(10, "0")
+    end
+  end
+end
+
+class Cell
+end
+
+# overkill? you betcha
+class Printer
 end
